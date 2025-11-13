c7f46c038874a769df25ea137dd4d9fe019f13b1
arthur <artuguen28@gmail.com>
2025-11-13 13:03:49 -0400

    feat(monitoring): Create Camera class to handle specific data about cameras and add a 5 min video to generate more data about the predictions

---
+++ traffic_counter/camera.py
@@ -0,0 +1,10 @@
+class Camera:
+    def __init__(self, cam_id, lanes, camera_video):
+        """
+        cam_id: unique camera identifier
+        lanes: list of tuples [(x1, y1, x2, y2), ...]
+            where (x1, y1) and (x2, y2) define the counting line segment.
+        """
+        self.id = cam_id
+        self.lanes = lanes
+        self.camera_video = camera_video
--- traffic_counter/main.py
+++ traffic_counter/main.py
@@ -4,22 +4,34 @@ from tracker import VehicleTracker
 from counter import LaneCounter
 from visualizer import Visualizer
 from database_client import DatabaseClient
+from camera import Camera
 
 def main(model_path, input_path, output_path):
+
+    lanes = [
+        (0, 1000, 650, 1000),       # lane 1
+        (800, 1000, 1100, 1000),    # lane 2
+        (1200, 1000, 1600, 1000),   # lane 3
+    ]
+
+    camera = Camera(
+        cam_id="CAM_01", 
+        lanes=lanes, 
+        camera_video=input_path
+    )
+    counter = LaneCounter(camera)
+    visualizer = Visualizer(camera.lanes)
     tracker = VehicleTracker(model_path)
-    line_y = 1000
-    lane_regions = [(0, 650), (800, 1100), (1200, 1600)]
-    counter = LaneCounter(lane_regions, line_y, "0001")
     db_client = DatabaseClient()
 
-    visualizer = Visualizer(line_y, lane_regions)
-
-    cap = cv2.VideoCapture(input_path)
+    # Initialize Open-cv
+    cap = cv2.VideoCapture(camera.camera_video)
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     w, h = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     writer = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-
     frame_count = 0
+
+    # Main Loop
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -34,16 +46,13 @@ def main(model_path, input_path, output_path):
 
         writer.write(frame)
 
-        
-
-        if counter.periodic_save(interval=7, db_client=db_client):
+        if counter.periodic_save(interval=10, db_client=db_client):
             print(f"[SAVE] Counts at {frame_count/fps:.1f}s → {counts}")
 
         frame_count += 1
         if frame_count % 30 == 0:
             print(f"Processed {frame_count} frames...")
-
-    db_client.close()
+        
 
     cap.release()
     writer.release()
